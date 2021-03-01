96b303cd6d7ac2441657ea77a610c6122f8379fe
Nick Hape <hape.nick@gmail.com>
2021-03-01 15:23:25 +0100

    created accordion component and applied styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: "Title 1",
+    content: "Content 1"
+  },
+  {
+    title: "Title 2",
+    content: "Content 2"
+  },
+  {
+    title: "Title 3",
+    content: "Content 3"
+  }
+];
 
 export default () => {
-  return <h1>Widgets App</h1>
+  return (
+      <div>
+        <Accordion items={items} />
+      </div>
+    );
+
 };
